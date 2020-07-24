4eb639da623e2d8825686a791fa588591cb8c19b
Volker Jaenisch <volker.jaenisch@inqbus.de>
2020-07-24 02:29:45 +0200

    mvc 1.0

--- mylcd.py
+++ mylcd.py
@@ -5,12 +5,16 @@ class Widget(object):
     
     _content = ''
     display = None
-    autorender = False
+    autorender = True
     
     def __init__(self, position, **kwargs):
         self.position = position
         if 'autorender' in kwargs:
             self.autorender = kwargs['autorender']
+        
+    def signal(self, signal, value=None):
+        print('Signal received', signal)
+        getattr(self, signal)()
         
     def render(self):
         pass
@@ -36,6 +40,11 @@ class Selectable(object):
 
     def __init__(self, selected_idx=0):
         self.selected_idx = selected_idx
+        self._signals = {
+            'up', self.on_up,
+            'down', self.on_down,
+            'click', self.on_click,
+            }
         
     @property
     def parent(self):
@@ -69,9 +78,11 @@ class Selectable(object):
             self.selected_idx -= 1
         else:
             self.parent.up()
+            
+    def on_signal(self, signal):
+        self._signals[signal]()
 
         
-    
 class Line(Widget):
     
     def render(self):
@@ -152,11 +163,18 @@ class Page(Widget):
         widget.display = self.display
         widget.parent = self
         self.widgets.append(widget)
+
+    def active_widget(self):
+        return self.widgets[0]
         
     def render(self):
         for widget in self.widgets:
             widget.render()
-    
+
+    def signal(self, signal, value=None):
+        print('Signal received', signal)
+        getattr(self.active_widget(), signal)()
+        
             
 class Display(object):
     
@@ -184,11 +202,81 @@ class Controller(object):
     
     _active_page = None
     
-    def __init__(self, active_page):
-        pass
+    @property
+    def active_page(self):
+        return self._active_page
+        
+    @active_page.setter
+    def active_page(self, value):
+        self._active_page = value
+        self._active_page.render()
+    
+    def up(self):
+        print('sended signal up')
+        self.active_page.signal('up')
+
+    def down(self):
+        print('sended signal down')
+        self.active_page.signal('down')
+
+    def click(self):
+        print('sended Signal click')
+        self.active_page.signal('click')
+    
+    def register_input(self, input_cls):
+        self.input = input_cls(
+            self.up,
+            self.down,
+            self.click,
+            )
+
+    def loop(self):
+        while True:
+            sleep(1)
+        
+        
+class Input(object):
+    up_handler = None
+    down_handler = None
+    click_handler = None
+
+
+from pigpio_encoder import Rotary
+from time import sleep
+    
+class InputRotary(Input):
+
+    rotary = None
+    counter = None
+    
+    def __init__(self, up_handler, down_handler, click_handler):
+        self.up_handler = up_handler
+        self.down_handler = down_handler
+        self.click_handler = click_handler
+        self.rotary = Rotary(clk=17, dt=22, sw=27)
+        self.rotary.setup_rotary(rotary_callback=self.rotary_callback)
+        self.rotary.setup_switch(sw_short_callback=self.click_callback)
+        
+    
+    def rotary_callback(self, counter):
+        print('Rotation', counter)
+        if not self.counter:
+            self.counter = counter
+            return
+        if counter > self.counter:
+            self.counter = counter
+            self.up_handler()
+        elif counter < self.counter:
+            self.counter = counter
+            self.down_handler()
+    
+    def click_callback(self):
+        print('Click!')
+        self.click_handler()
     
             
 display = Display()
+controller = Controller()
 
 start_page = Page(display)
 
@@ -198,11 +286,12 @@ body = Select((1,0), 3, Selectable())
 start_page.add_widget(header)
 start_page.add_widget(body)
 
-
 header.content = 'Hallo'
 body.content = ['Volker', 'Du', 'Aas']
 
-
 header.content = 'Huhu'
 
-start_page.render()
+controller.active_page = start_page
+controller.register_input(InputRotary)
+
+controller.loop()
