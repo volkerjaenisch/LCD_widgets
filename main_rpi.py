505b403cabc1a9650b548692f80f8b362e102227
Volker Jaenisch (PhD) <volker.jaenisch@inqbus.de>
2020-08-13 21:44:55 +0200

    RPI main file

---
+++ main_rpi.py
@@ -0,0 +1,45 @@
+from inqbus.rpi.widgets.base.display import Display
+from inqbus.rpi.widgets.input.rotary_encoder import RotaryInput
+from inqbus.rpi.widgets.interfaces.widgets import IGUI
+from inqbus.rpi.widgets.widgets import Page, Line, Select
+
+from zope.component import getUtility
+
+import inqbus.rpi.widgets.gui # IMPORTANT!
+import inqbus.rpi.widgets.render # IMPORTANT!
+import inqbus.rpi.widgets.widgets # IMPORTANT!
+import inqbus.rpi.widgets.base.controller # IMPORTANT!
+import inqbus.rpi.widgets.base.notify # IMPORTANT!
+
+
+gui = getUtility(IGUI)
+
+display = Display()
+gui.add_display(display)
+
+input = RotaryInput()
+gui.add_input(input)
+
+gui.init()
+
+
+layout = Page()
+
+line = Line()
+line.content = 'huhu'
+layout.add_widget(line)
+
+select = Select(pos_y=1, line_count=4)
+select.content = [
+    'sel1',
+    'sel2',
+    'sel3',
+    'sel4',
+]
+
+layout.add_widget(select)
+
+gui.set_layout(layout)
+
+
+gui.run()
