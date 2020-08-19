e95e99004f0be0e8ff39c48236d4a5d506f28f01
volker <volker.jaenisch@inqbus.de>
2020-08-20 01:05:07 +0200

    added sphinx

--- main_rpi.py
+++ main_rpi.py
@@ -1,45 +1,60 @@
-from inqbus.rpi.widgets.base.display import Display
+from inqbus.rpi.widgets.display.console import ConsoleDisplay
+from inqbus.rpi.widgets.display.rplcd_display import RPLCDDisplay
 from inqbus.rpi.widgets.input.rotary_encoder import RotaryInput
-from inqbus.rpi.widgets.interfaces.widgets import IGUI
-from inqbus.rpi.widgets.widgets import Page, Line, Select
+from inqbus.rpi.widgets.interfaces.interfaces import IGUI
+from inqbus.rpi.widgets.widgets import Page, Line, Lines, Select, Button
 
 from zope.component import getUtility
 
 import inqbus.rpi.widgets.gui # IMPORTANT!
 import inqbus.rpi.widgets.render # IMPORTANT!
 import inqbus.rpi.widgets.widgets # IMPORTANT!
+import inqbus.rpi.widgets.controllers # IMPORTANT!
 import inqbus.rpi.widgets.base.controller # IMPORTANT!
 import inqbus.rpi.widgets.base.notify # IMPORTANT!
+import inqbus.rpi.widgets.base.focus # IMPORTANT!
 
+def button_clicked():
+    print('Button Clicked!')
+    return True
+
+layout = Page()
+
+line = Button()
+line.content = 'huhu' + '↑'
+line.click_handler = button_clicked
+
+layout.add_widget(line)
+
+text = Lines()
+text.content = ["Hallo",
+                "Welt"]
+
+select = Select(pos_y=1, height=3)
+select.content = [
+    'sel1',
+    text,
+    'sel3',
+    'sel4',
+    'sel5',
+    'sel6',
+    'sel7',
+]
+
+layout.add_widget(select)
 
 gui = getUtility(IGUI)
-
-display = Display()
+display = ConsoleDisplay()
+gui.add_display(display)
+display = RPLCDDisplay(4, 20, 'PCF8574', 0x27)
 gui.add_display(display)
 
 input = RotaryInput()
 gui.add_input(input)
 
-gui.init()
-
-
-layout = Page()
-
-line = Line()
-line.content = 'huhu'
-layout.add_widget(line)
-
-select = Select(pos_y=1, line_count=4)
-select.content = [
-    'sel1',
-    'sel2',
-    'sel3',
-    'sel4',
-]
-
-layout.add_widget(select)
-
 gui.set_layout(layout)
 
+gui.focus = line
 
+gui.init()
 gui.run()
