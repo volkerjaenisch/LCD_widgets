33e22684488c02eb62b04c11965af5d9e2e4f684
volker <volker.jaenisch@inqbus.de>
2020-08-31 20:50:02 +0200

    Renderer and tests for Text

--- src/inqbus/rpi/widgets/text.py
+++ src/inqbus/rpi/widgets/text.py
@@ -1,6 +1,9 @@
+from inqbus.rpi.widgets.base.render import Renderer
 from inqbus.rpi.widgets.base.widget import Widget
+from inqbus.rpi.widgets.interfaces.interfaces import IRenderer
 from inqbus.rpi.widgets.interfaces.widgets import ITextWidget
-from zope.interface import implementer
+from zope.component import getGlobalSiteManager
+from zope.interface import implementer, Interface
 
 
 @implementer(ITextWidget)
@@ -12,3 +15,40 @@ class Text(Widget):
         assert isinstance(value, str)
         super(Text, self).handle_new_content(value)
 
+
+@implementer(IRenderer)
+class TextRenderer(Renderer):
+    """
+    Renderer for a LinesWidget
+    """
+    __used_for__ = (ITextWidget, Interface)
+
+    def render(self):
+        pos_x = self.widget.pos_x
+        pos_y = self.widget.pos_y
+
+        if self.widget.width:
+            width = self.widget.width
+        else:
+            width = self.display.width - pos_x
+
+        content = self.widget.content
+        start_pos = 0
+
+        while True:
+            rest_len = len(content[start_pos:])
+            if width > rest_len:
+                self.display.write_at_pos(pos_x, pos_y, content[start_pos:start_pos + width])
+                break
+            else:
+                self.display.write_at_pos(pos_x, pos_y, content[start_pos:start_pos + width])
+                start_pos += width
+            pos_y += 1
+        # return the coordinate after the content
+        # ToDo width, height handling
+        return pos_x, pos_y + 1
+
+
+# Register the render adapter
+gsm = getGlobalSiteManager()
+gsm.registerAdapter(TextRenderer, (ITextWidget, Interface), IRenderer)
